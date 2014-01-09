de304f5ff4a99761554920e63c89d1ab039c74a6
Timothy Scott <timothyscott@ksu.edu>
2014-01-09 09:23:42 -0600

    work on task 2 and 4

---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,82 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Timothy Scott on 12/23/13.
+//  Copyright (c) 2013 Eternal Enigma. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@interface CardMatchingGame()
+@property (nonatomic, readwrite) NSInteger score;
+@property (nonatomic, strong) NSMutableArray *cards; // of Card
+@property (nonatomic, readwrite) NSString *matchMode;
+@end
+
+@implementation CardMatchingGame
+
+- (NSMutableArray *)cards
+{
+    if (! _cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+- (instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck
+{
+    self = [super init];
+    self.score = 0;
+    
+    if (self) {
+        for (int i = 0; i < count; i++) {
+            Card *card = [deck drawRandomCard];
+            if (card) {
+                [self.cards addObject:card];
+            } else {
+                self = nil;
+                break;
+            }
+        }
+    }    
+    return self;
+}
+
+- (Card *)cardAtIndex:(NSUInteger)index
+{
+    return (index<[self.cards count]) ? self.cards[index] : nil;
+}
+
+static const int MISMATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+static const int COST_TO_CHOOSE = 1;
+
+- (void)chooseCardAtIndex:(NSUInteger)index
+{
+    Card *card = [self cardAtIndex:index];
+    
+    if (!card.isMatched) {
+        if (card.isChosen) {
+            card.chosen = NO;
+        } else {
+            // Match against other chosen cards
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        self.score += matchScore * MATCH_BONUS;
+                        card.matched = YES;
+                        otherCard.matched = YES;
+                    } else {
+                        self.score -= MISMATCH_PENALTY;
+                        otherCard.chosen = NO;
+                    }
+                    break; // can only choose 2 cards for now
+                }
+            }
+            self.score -= COST_TO_CHOOSE;
+            card.chosen = YES;
+        }
+    }
+}
+
+@end
